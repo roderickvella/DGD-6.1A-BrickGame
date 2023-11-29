8c34f5ad3ebcfa435326001a48ca5a17b75bbbe7
Roderick Vella Galea <roderick.vella@mcast.edu.mt>
2023-11-29 09:18:32 +0100

    Lesson1
    drawing the brick and drawing a ball (with movement animation)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,52 @@
 #include "raylib.h"
 
+class Brick {
+    public:
+        Vector2 position; //Holds the x and y position of the brick
+        Vector2 size; //Holds the width and height of the brick
+        Color color; //Holds the color of the brick
+
+        //Constructor of the brick class
+        Brick(Vector2 position, Vector2 size, Color color) {
+            this->position = position;
+            this->size = size;
+            this->color = color;
+        }
+
+        //Draws the brick
+        void Draw(){
+            DrawRectangleV(position, size, color);
+        }
+
+};
+
+class Ball{
+    public:
+        Vector2 position; //Holds the x and y position of the ball
+        float radius; //Holds the radius of the ball
+        Color color; //Holds the color of the ball
+        Vector2 speed; //Holds the speed of the ball
+
+        //Constructor of the ball class
+        Ball(Vector2 position, float radius, Color color, Vector2 speed) {
+            this->position = position;
+            this->radius = radius;
+            this->color = color;
+            this->speed = speed;
+        }
+
+        //Draws the ball
+        void Draw(){
+            DrawCircleV(position, radius, color);
+        }
+
+        void Update(){
+            //modify the position of the ball based on its speed
+            position.x += speed.x * GetFrameTime();
+            position.y += speed.y * GetFrameTime();
+        }
+};
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -7,20 +54,26 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 900;
-    const int screenHeight = 450;
+    const int screenWidth = 800;
+    const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Brick Game");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
+
+    //create a single brick instance
+    Brick brick({200,50},{100,50},RED);
+
+    //create a single ball instance
+    Ball ball({400,300},10,RED,{50,50});
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        ball.Update(); //update the ball
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -29,7 +82,8 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            brick.Draw(); //draw the brick
+            ball.Draw(); //draw the ball
 
         EndDrawing();
         //----------------------------------------------------------------------------------
